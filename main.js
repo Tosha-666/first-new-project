9f05d206f02a907dcabd5ed5921de845532ace95
Tosha-666 <qwerty21042018@mail.ru>
2021-07-09 17:41:53 +0300

    добавлены слайдеры

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ const swiper = new Swiper(".swiper-container", {
 const repairBrands = document.querySelector('.repair-brands-wrapper__showed-768px');
 
 
-const showMoreButton = document.querySelector('.show-more__about');
+const showMoreButton = document.querySelector('.show-more__aboutBrands');
 
 //const pageWidth = document.documentElement.scrollWidth;
 //console.log(pageWidth);
@@ -51,6 +51,23 @@ const showMoreButton = document.querySelector('.show-more__about');
 
 showMoreButton.addEventListener('click', () => {
       repairBrands.classList.toggle('showed');
+  }
+ );
+
+console.log(repairBrands);
+
+
+const repairBrandsTechnicTypes = document.querySelector('.repair-brands-wrapper__showed-768px--technictypes');
+
+
+const showMoreButtonTechnicTypes = document.querySelector('.show-more__about--technictypes');
+
+//const pageWidth = document.documentElement.scrollWidth;
+//console.log(pageWidth);
+
+
+showMoreButtonTechnicTypes.addEventListener('click', () => {
+      repairBrandsTechnicTypes.classList.toggle('showed');
   }
  );
 
