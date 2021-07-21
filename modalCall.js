db58c4cd70fc29ba3dc51171cc1e65f89499bfba
Tosha-666 <qwerty21042018@mail.ru>
2021-07-21 18:25:22 +0300

    редактирование js

--- modalCall.js
+++ modalCall.js
@@ -1,6 +1,9 @@
 const callButton = document.querySelectorAll('.modal__call--button');
 const modalCall = document.querySelector('.modal__call');
-//const commonContainerBlurCall = document.querySelector('.common-container ');
+const commonContainerBlurCall = document.querySelector('.common-container ');
+const closeModalCall = document.querySelector('.call-close');
+
+
 
 
 for (let i = 0; i < callButton.length; i++){
@@ -11,20 +14,44 @@ for (let i = 0; i < callButton.length; i++){
       }
  );
 }
+
+
+
+closeModalCall.addEventListener('click', (e) => {
+  //console.log(e.target);
+ //if (modalCall.classList.contains ('active__call')) {
+ // if (e.target !== modalCall) {
+    console.log(e.target);
+      modalCall.classList.remove('active__call');
+     commonContainerBlurFeedback.classList.remove('blur')
+      /*closeModal();
+      mobileMenu.classList.add('mobile-menu-close');
+      mobileMenu.classList.remove('mobile-menu-active');
+      document.body.style.overflow = '';
+      overlayMenu.classList.remove('overlay');*/
+  // }
+  
+  //}
+ //return
+  });
 /*const closeModalCall = function () {
     modalCall.classList.remove('active__call');
     commonContainerBlurFeedback.classList.remove('blur')
  }
+*/
 
-function outsideEvtListener(e) {
-    if (e.target === modalCall || modalCall.contains(e.target)) {
-        return;
-    }
-    modalCall.classList.remove('active__call');
-    commonContainerBlurFeedback.classList.remove('blur')
-  
-    document.removeEventListener(outsideEvtListener);
-}
-
-
+/*function outsideEvtListener(evt) {
+    if (modalCall.className == 'active__call'){
+        if (evt.target === modalCall || modalCall.contains(evt.target)) {
+        console.log(modalCall)
+            return
+        } 
+            modalCall.classList.remove('active__call');
+            commonContainerBlurFeedback.classList.remove('blur')
+            /*document.removeEventListener(outsideEvtListener);
+        }
+        
+  }
+       
 document.addEventListener('click', outsideEvtListener);*/
+
--- modalFeedback.js
+++ modalFeedback.js
@@ -1,6 +1,7 @@
 const feedbackButton = document.querySelectorAll('.modal__feedback--button');
 const modalFeedback = document.querySelector('.modal__feedback');
 const commonContainerBlurFeedback = document.querySelector('.common-container ');
+const closeModalFeedback= document.querySelector('.feedback-close');
 //const pageWidth = document.documentElement.scrollWidth;
 //console.log(pageWidth);
 
@@ -13,5 +14,21 @@ for (let i = 0; i < feedbackButton.length; i++){
 }
 
 
-
+closeModalFeedback.addEventListener('click', (e) => {
+  //console.log(e.target);
+ //if (modalCall.classList.contains ('active__call')) {
+ // if (e.target !== modalCall) {
+    console.log(e.target);
+    modalFeedback.classList.remove('active__feedback');
+    commonContainerBlurFeedback.classList.remove('blur')
+      /*closeModal();
+      mobileMenu.classList.add('mobile-menu-close');
+      mobileMenu.classList.remove('mobile-menu-active');
+      document.body.style.overflow = '';
+      overlayMenu.classList.remove('overlay');*/
+  // }
+  
+  //}
+ //return
+  });
 
